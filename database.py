f3dd7c38b98bd1b83310d7246d21c61ad3679f28
MichaelaKerr <michaela.kerr@countiesenergy.co.nz>
2026-05-01 09:08:19 +1200

    expire on commit

--- database.py
+++ database.py
@@ -14,7 +14,7 @@ def get_engine():
 @contextmanager
 def get_db():
     engine = get_engine()
-    Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+    Session = sessionmaker(bind=engine, autocommit=False, autoflush=False, expire_on_commit=True)
     session = Session()
     try:
         yield session
